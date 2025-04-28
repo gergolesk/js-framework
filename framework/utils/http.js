dd3cdf568415f10085eafa8b85301c4e075332ee
gergolesk <tampere1737@mail.ru>
2025-04-28 15:58:33 +0300

    HTTP. Fix some problems

--- framework/utils/http.js
+++ framework/utils/http.js
@@ -6,35 +6,7 @@
  * 
  * const user = await httpRequest('/api/user', 'POST', { name: 'Alice' });
  */
-/*
-export async function httpRequest(url, method = 'GET', data = null) {
-  // Build the configuration object for the fetch request
-  const config = {
-    method,
-    headers: {
-      'Content-Type': 'application/json' // Send data as JSON
-    }
-  };
 
-  // If data is provided (for POST, PUT, etc), include it in the request body
-  if (data) {
-    config.body = JSON.stringify(data);
-  }
-
-  // Perform the HTTP request
-  const res = await fetch(url, config);
-
-  // Throw an error if the response is not successful (status not in 200–299)
-  if (!res.ok) {
-    throw new Error(`HTTP error! status: ${res.status}`);
-  }
-
-  // Parse and return the response body as JSON
-  return res.json();
-
-  
-} 
-  */
 export async function httpRequest(url, method = 'GET', data = null) {
   const config = {
     method,
